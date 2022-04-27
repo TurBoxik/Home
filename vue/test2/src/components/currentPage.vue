9e714c56416472d9178efa0f2ca056ae1213a3d8
TurBoxik <mikolaj_jankowski98@wp.pl~>
2022-04-27 15:37:05 +0200

    270422b

---
+++ vue/test2/src/components/currentPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <pagination
+      :totalPages="10"
+      :perPage="10"
+      :currentPage="currentPage"
+      @pagechanged="onPageChange"
+    />
+  </div>
+</template>
+
+<script>
+import Pagination from './components/Pagination.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Pagination
+  },
+  data () {
+    return {
+      currentPage: 1,
+    };
+  },
+  methods: {
+    onPageChange(page) {
+      console.log(page)
+      this.currentPage = page;
+    }
+  }
+}
+</script>
